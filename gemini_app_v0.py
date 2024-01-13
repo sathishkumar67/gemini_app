2a0bf5b2bc63ef08ec35468f7e6f8bc0c52d2273
Sathish Kumar <90573412+sathishkumar67@users.noreply.github.com>
2024-01-13 16:07:59 +0530

    Update gemini_app_v0.py

--- gemini_app_v0.py
+++ gemini_app_v0.py
@@ -1,6 +1,6 @@
 # import pyttsx3
 import streamlit as st
-# from gemini_pro import gemini_pro_text
+from gemini_pro import gemini_pro_text
 # from speechrecognition import record_and_convert_to_text
 
 
@@ -8,9 +8,9 @@ import streamlit as st
 # engine = pyttsx3.init()
 
 text = st.chat_input("Enter What you would like to Know")
-# if text:
-    # response = gemini_pro_text.generate_content([text])
-    # st.write(response.text)
+if text:
+    response = gemini_pro_text.generate_content([text])
+    st.write(response.text)
 
 # if st.button("Record Audio"):
 #     audio_text = record_and_convert_to_text()
